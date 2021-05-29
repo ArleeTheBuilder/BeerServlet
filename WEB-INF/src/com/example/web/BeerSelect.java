d707753b56784fe3bcb89853e8c91c9062c22deb
arlee <arleethebuilder163@gmail.com>
2021-05-29 09:33:41 +0800

    First Commit, Compiled, Tested

---
+++ WEB-INF/src/com/example/web/BeerSelect.java
@@ -0,0 +1,18 @@
+package com.example.web;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+
+public class BeerSelect extends HttpServlet {
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException {
+
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			out.println("Beer Selection Advice<br>");
+			String c = request.getParameter("color");
+			out.println("<br>Got beer color " + c);
+		}
+}
